29c9f721c7494c0fb7577c5a2e6d19f957f0614b
Andrei Sharov <sharov89@gmail.com>
2025-11-13 02:21:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/study_fragments/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.study_fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            // в этот момент мы отображаем Fragment
+
+            supportFragmentManager.commit {
+                add<CitiesFragment>(R.id.fragment_container_view)
+            }
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container_view)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
