d950e075f25ca0f3b97a0612de5f5812147ae6df
Andrei <sharov89@gmail.com>
2025-11-13 17:57:50 +0300

    fragments task

--- app/src/main/java/com/example/study_fragments/MainActivity.kt
+++ app/src/main/java/com/example/study_fragments/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.study_fragments.databinding.ActivityMainBinding
 
@@ -18,33 +17,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         enableEdgeToEdge()
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<CitiesFragment>(R.id.fragment_container_view)
-            }
-        }
+        /*        if (savedInstanceState == null) {
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+                        add<CitiesFragment>(binding.fragmentContainerView.id)
+                    }
+                }*/
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.fragmentContainerView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setOnClickListeners()
+        //setOnClickListeners()
 
     }
     private fun setOnClickListeners() {
         binding.replaceCountryFragment.setOnClickListener {
             supportFragmentManager.commit {
-                replace(R.id.fragment_container_view, CountriesFragment())
+                replace(binding.fragmentContainerView.id, CountriesFragment())
                 setReorderingAllowed(true)
             }
 
         }
         binding.replaceBackStackCountryFragment.setOnClickListener {
             supportFragmentManager.commit {
-                replace(R.id.fragment_container_view, CountriesFragment())
-                addToBackStack(null)
+                replace(binding.fragmentContainerView.id, CountriesFragment())
+                addToBackStack("my_backstack")
                 setReorderingAllowed(true)
             }
         }
---
+++ app/src/main/java/com/example/study_fragments/RegFragment.kt
@@ -0,0 +1,23 @@
+package com.example.study_fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.study_fragments.databinding.FragmentRegBinding
+
+class RegFragment : Fragment() {
+
+    private var _binding: FragmentRegBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
